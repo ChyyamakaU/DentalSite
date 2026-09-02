b32e811268585ff93271b17413d71c4dfc7d4091
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 15:39:17 +0100

    ft: add color to hero text

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,9 +8,9 @@ function Hero() {
         <img src={HeroImage} className="hidden md:block object-contain rounded-3xl w-3/6" alt="" />
 
         <div className="text-center pt-20 ">
-          <h1 className="text-3xl md:text-6xl font-semibold ">Dental Care for Healthy Smiles</h1>
+          <h1 className="text-3xl md:text-6xl font-semibold text-[#12345B] ">Dental Care for Healthy Smiles</h1>
 
-          <p className="text-lg md:text-2xl pt-5 text-[]">Experience stress free dentistry focuced <br /> on your long term oral health</p>
+          <p className="text-lg md:text-2xl pt-5 text-[#000000]">Experience stress free dentistry focuced <br /> on your long term oral health</p>
         
          <button className=" bg-[#12345B] text-[#ffffff]  rounded-lg text-xs md:text-lg px-4 py-2 my-6 md:my-10">
             Book Appointment
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,25 +1,18 @@
 function Services() {
- 
-
   return (
     <>
-      
       <section>
-<h2 className="text-4xl text-center font-semibold text-[#12345B]">OUR SERVICES</h2>
-<div>
-  <h2></h2>
-</div>
+        <h2 className="text-4xl text-center font-semibold text-[#12345B]">
+          OUR SERVICES
+        </h2>
+        <div>
+          <h3>
 
-
-
-
-
-
+          </h3>
+        </div>
       </section>
-
-      
     </>
-  )
+  );
 }
 
-export default Services
+export default Services;
