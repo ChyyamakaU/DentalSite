b1b2cffd2e8854ab0ee6a6c60f6025375378380b
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 15:06:48 +0100

    ft: add the menu icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,9 @@ function Header() {
       <header className=" flex justify-between items-center px-10 py-4 bg-[#EAF3FA]">
         <div className="flex items-center border-2 border-[#12345B] text-xs md:text-lg rounded-3xl px-1  md:px-2 md:py-1">
           <i className="fa-solid fa-tooth text-[#12345B] text-xs md:text-2xl "></i>
-          <p className="text-[#2F6FAE] text-xs md:text-lg font-semibold font-mono">SMILE</p>
+          <p className="text-[#2F6FAE] text-xs md:text-lg font-semibold font-mono">
+            SMILE
+          </p>
         </div>
 
         <nav className="text-[#1A2B3D] hover:[#2F6FAE] hidden md:flex gap-6 text-xl font-medium">
@@ -15,7 +17,12 @@ function Header() {
         </nav>
 
         <div>
-          <button className="bg-[#12345B] text-[#ffffff] rounded-lg text-xs md:text-lg px-4 py-2">Book Appointment</button>
+          <button className="block md:hidden">
+            <i className="fa-solid fa-bars" ></i>
+          </button>
+          <button className="hidden md:block bg-[#12345B] text-[#ffffff]  rounded-lg text-xs md:text-lg px-4 py-2">
+            Book Appointment
+          </button>
         </div>
       </header>
     </>
