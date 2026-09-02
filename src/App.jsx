de452c1b0edbf2f4b4b961c8cef2a0d978b4ed53
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 15:37:00 +0100

    ft: add our services heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './Components/Header'
 import Hero from './Components/Hero'
+import Services from './Components/Services'
 import Footer from './Components/Footer'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Header/>
       <Hero/>
+      <Services/>
       <Footer/>
       
     </>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,6 +4,19 @@ function Services() {
   return (
     <>
       
+      <section>
+<h2 className="text-4xl text-center font-semibold text-[#12345B]">OUR SERVICES</h2>
+<div>
+  <h2></h2>
+</div>
+
+
+
+
+
+
+      </section>
+
       
     </>
   )
