b2ef182d1aa3d2e1f594f5eb23a6e02bd05992fb
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 16:19:22 +0100

    ft: add team photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Header'
 import Hero from './Components/Hero'
 import Services from './Components/Services'
+import Team from './Components/Team'
 import Footer from './Components/Footer'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Hero/>
       <Services/>
+      <Team/>
       <Footer/>
       
     </>
--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,12 +1,33 @@
+import Aa from "../assets/DrA.jpg";
+import Ba from "../assets/DrB.jpg";
+
 function Team() {
-
-
   return (
     <>
-      
-      
+      <section>
+        <h2 className="text-2xl md:text-4xl py-8 md:py-10 text-center font-semibold text-[#12345B]">
+          OUR Team
+        </h2>
+
+        <div>
+          <div>
+            <img className="w-1/6 rounded-2xl " src={Aa} alt="Doctor A" />
+            <p>Doctor John Doe</p>
+          </div>
+
+          <div>
+            <img src={Ba} alt="Doctor B" />
+            <p>Doctor Jane Doe</p>
+          </div>
+
+          <div>
+            <img className="w-1/6 rounded-2xl " src={Aa} alt="Doctor A" />
+            <p>Doctor John Doe</p>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Team
+export default Team;
