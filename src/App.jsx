0bf218fef99394eadd66e2297c06cd39f3e7f54a
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 14:05:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './Components/Header'
+import Hero from './Components/Hero'
+import Footer from './Components/Footer'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Footer/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+
+
+function Hero() {
+ 
+
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default Hero
