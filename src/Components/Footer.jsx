5e6755a890b1ca50aca087502184a6fe8d113900
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 23:34:26 +0100

    ft: add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,52 +1,58 @@
 function Footer() {
   return (
     <>
-      <footer className="grid grid-cols-1 md:grid-cols-4 gap-5">
+      <footer className="grid grid-cols-1 md:grid-cols-5 gap-12 px-6 md:px-10 bg-[#12345B] py-8 md:py-16">
         <div>
-          <div className="flex items-center border-2 border-[#12345B] text-xs md:text-lg rounded-3xl px-1  md:px-2 md:py-1">
-            <i className="fa-solid fa-tooth text-[#12345B] text-xs md:text-2xl "></i>
+          <div className="flex items-center border-2 border-[#99a8b8] text-xs w-28 md:text-lg rounded-3xl px-1  md:px-2 md:py-1">
+            <i className="fa-solid fa-tooth text-[#9bb7d4] text-xs md:text-2xl "></i>
             <p className="text-[#2F6FAE] text-xs md:text-lg font-semibold font-mono">
               SMILE
             </p>
           </div>
 
-          <p>Get Your Smile back</p>
+          <p className="text-[#DCE6EF] pt-1 md:pt3">Get Your Smile back</p>
         </div>
 
         <div>
-          <h3>Contact</h3>
+          <h3 className="text-[#FFFFFF] font-semibold">Contact</h3>
 
-          <div className="flex gap-1 text-sm items-center ">
+          <div className="flex gap-1 text-sm md:text-lg items-center text-[#DCE6EF]">
             <i class="fa-solid fa-phone"></i>
             <p>08123456789</p>
           </div>
 
-          <div className="flex gap-1 text-sm items-center ">
+          <div className="flex gap-1 text-sm md:text-lg items-center text-[#DCE6EF] ">
             <i class="fa-regular fa-envelope"></i>
             <p>enquiries@smileclinics.com</p>
           </div>
 
-          <div className="flex gap-1 text-sm items-center ">
+          <div className="flex gap-1 text-sm md:text-lg items-center text-[#DCE6EF]">
             <i class="fa-solid fa-location-dot"></i>
             <p>XYZ Street, Jos</p>
           </div>
         </div>
 
         <div>
-          <h3>Opening Hours</h3>
-          <div>
+          <h3 className="text-[#FFFFFF] font-semibold">Opening Hours</h3>
+          <div className="text-[#DCE6EF] text-sm md:text-lg">
             <p>Monday- Friday: 9:00am to 5:00pm</p>
             <p>Saturday: 11:00am to 2:00pm</p>
             <p>Sunday: Closed</p>
           </div>
         </div>
 
+        <div>
+          <h3 className="text-[#FFFFFF] font-semibold">Social Handles</h3>
+
+          <div className="text-sm md:text-lg"></div>
+        </div>
 
         <div>
-          <h3>Social Handles</h3>
+          <h3 className="text-[#FFFFFF] font-semibold">Links</h3>
 
-          <div>
-            
+          <div className="text-[#DCE6EF] text-sm md:text-lg">
+            <p>Privacy</p>
+            <p>Terms and Conditions</p>
           </div>
         </div>
       </footer>
