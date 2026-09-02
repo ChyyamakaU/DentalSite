0506a84b37cf79d5191367aec29923af1e5dd71d
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 15:32:16 +0100

    ft: add call to action button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,16 +5,21 @@ function Hero() {
     <>
     <section className="bg-gray-100">
       <div className="flex justify-between gap-5 px-10 md:py-24 ">
-        <img src={HeroImage} className="hidden md:block rounded-3xl w-3/6" alt="" />
+        <img src={HeroImage} className="hidden md:block object-contain rounded-3xl w-3/6" alt="" />
 
         <div className="text-center pt-20 ">
           <h1 className="text-3xl md:text-6xl font-semibold ">Dental Care for Healthy Smiles</h1>
 
           <p className="text-lg md:text-2xl pt-5 text-[]">Experience stress free dentistry focuced <br /> on your long term oral health</p>
+        
+         <button className=" bg-[#12345B] text-[#ffffff]  rounded-lg text-xs md:text-lg px-4 py-2 my-6 md:my-10">
+            Book Appointment
+          </button>
+        
         </div>
 
-
       </div>
+      
       </section>
     </>
   );
