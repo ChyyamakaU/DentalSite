17e2ae7b6683dbc532f9c9b8cc936afbfaa2cf53
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-02 16:03:56 +0100

    ft:  add learn more text and icon

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -7,45 +7,61 @@ function Services() {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-4 md:px-10 gap-5">
-          <div>
-            <h3 className="text-lg md:text-xl font-semibold text-center text-[#12345B] pb-2">General Dentistry</h3>
-            <p className="text-sm px-6 text-center text-[#26282b]">
-              Lorem ipsum dolor sit, amet consectetur adipisicing 
-              elit. Ipsum autem sunt odit totam. Harum  qui tenetur beatae
-              perferendis vit
+          <div className="text-center">
+            <h3 className="text-lg md:text-xl font-semibold  text-[#12345B] pb-2">
+              General Dentistry
+            </h3>
+            <p className="text-sm px-6  text-[#26282b]">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+              autem sunt odit totam. Harum qui tenetur beatae perferendis vit
             </p>
 
-            <div className="text-[#12345B] flex text-center ">
+            <div className="text-[#12345B] flex items-center text-xs md:text-sm py-2 gap-1 justify-center  ">
               <p>Learn More</p>
-            <i class="fa-solid fa-arrow-right"></i>
+              <i class="fa-solid fa-arrow-right"></i>
             </div>
           </div>
 
-          <div>
-            <h3 className="text-xl font-semibold text-center text-[#12345B] pb-2">Cosmetic Dentistry</h3>
-             <p className="text-sm px-6 text-center text-[#26282b]">
-              Lorem ipsum dolor sit, amet consectetur adipisicing 
-              elit. Ipsum autem sunt odit totam. Harum  qui tenetur beatae
-              perferendis vit
+          <div className="text-center">
+            <h3 className="text-xl font-semibold  text-[#12345B] pb-2">
+              Cosmetic Dentistry
+            </h3>
+            <p className="text-sm px-6  text-[#26282b]">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+              autem sunt odit totam. Harum qui tenetur beatae perferendis vit
             </p>
+            <div className="text-[#12345B] flex items-center text-xs md:text-sm py-2 gap-1 justify-center  ">
+              <p>Learn More</p>
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
           </div>
 
-          <div>
-            <h3 className="text-xl font-semibold text-center text-[#12345B] pb-2">Othodontics</h3>
-             <p className="text-sm px-6 text-center text-[#26282b]">
-              Lorem ipsum dolor sit, amet consectetur adipisicing 
-              elit. Ipsum autem sunt odit totam. Harum  qui tenetur beatae
-              perferendis vit
+          <div className="text-center">
+            <h3 className="text-xl font-semibold text-[#12345B] pb-2">
+              Othodontics
+            </h3>
+            <p className="text-sm px-6 text-[#26282b]">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+              autem sunt odit totam. Harum qui tenetur beatae perferendis vit
             </p>
+            <div className="text-[#12345B] flex items-center text-xs md:text-sm py-2 gap-1 justify-center  ">
+              <p>Learn More</p>
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
           </div>
 
-          <div>
-            <h3 className="text-xl font-semibold text-center text-[#12345B] pb-2">Preventive Care</h3>
-            <p className="text-sm px-6 text-center text-[#26282b]">
-              Lorem ipsum dolor sit, amet consectetur adipisicing 
-              elit. Ipsum autem sunt odit totam. Harum  qui tenetur beatae
-              perferendis vit
+          <div className="text-center ">
+            <h3 className="text-xl font-semibold text-[#12345B] pb-2">
+              Preventive Care
+            </h3>
+            <p className="text-sm px-6 text-[#26282b]">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
+              autem sunt odit totam. Harum qui tenetur beatae perferendis vit
             </p>
+            <div className="text-[#12345B] flex items-center text-xs md:text-sm py-2 gap-1 justify-center  ">
+              <p>Learn More</p>
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
           </div>
         </div>
       </section>
